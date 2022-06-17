5b7cf90c1078a6858d0ef654b4e0677c97597fde
petrostrak <pit.trak@gmail.com>
2022-06-17 20:06:02 +0300

    Weekly income project

--- 02-Mutex-and-Intro-to-Channels/main.go
+++ 02-Mutex-and-Intro-to-Channels/main.go
@@ -6,29 +6,51 @@ import (
 )
 
 var (
-	wg  sync.WaitGroup
-	msg string
+	wg sync.WaitGroup
 )
 
-func updateMessage(s string, m *sync.Mutex) {
-	defer wg.Done()
-
-	m.Lock()
-	msg = s
-	m.Unlock()
+type Income struct {
+	Source string
+	Amount int
 }
 
 // go run -race .
 func main() {
-	msg = "Hello, world!"
+	// variable for bank balance
+	var bankBalance int
+	var balance sync.Mutex
 
-	var m sync.Mutex
+	// print out starting values
+	fmt.Printf("Initial account balance: %d.00\n", bankBalance)
 
-	wg.Add(2)
-	go updateMessage("Hello, universe!", &m)
-	go updateMessage("Hello, cosmos!", &m)
+	// define weekly revenue
+	incomes := []Income{
+		{Source: "Main job", Amount: 500},
+		{Source: "Part-time job", Amount: 50},
+		{Source: "Investments", Amount: 100},
+		{Source: "Gifts", Amount: 10},
+	}
+
+	// loop through 52 and print out how much is made
+	// keep a running total
+	for i, income := range incomes {
+		wg.Add(1)
+		go func(i int, income Income) {
+			defer wg.Done()
+			for week := 1; week <= 52; week++ {
+				balance.Lock()
+				temp := bankBalance
+				temp += income.Amount
+				bankBalance = temp
+				balance.Unlock()
+
+				fmt.Printf("On week %d, you earned $%d.00 from %s\n", week, income.Amount, income.Source)
+			}
+		}(i, income)
+	}
 
 	wg.Wait()
 
-	fmt.Println(msg)
+	// print out final balance
+	fmt.Printf("Final bank balance: $%d.00\n", bankBalance)
 }
