c70112163c83af94fe0a41b3ad0135e9c72b2d86
petrostrak <pit.trak@gmail.com>
2022-06-18 15:17:41 +0300

    Making a pizza: the makePizza function

--- 03-Producer-Consumer-Problem/main.go
+++ 03-Producer-Consumer-Problem/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 
@@ -34,14 +35,60 @@ func (p *Producer) Close() error {
 	return <-ch
 }
 
+func makePizza(pizzaNumber int) *PizzaOrder {
+	pizzaNumber++
+	if pizzaNumber <= numberOfPizzas {
+		delay := rand.Intn(5) + 1
+		fmt.Printf("Received order #%d!\n", pizzaNumber)
+
+		rnd := rand.Intn(12) + 1
+		msg := ""
+		success := false
+
+		if rnd < 5 {
+			pizzasFailed++
+		} else {
+			pizzasMade++
+		}
+		total++
+		fmt.Printf("Making pizza #%d. It will take %d seconds...\n", pizzaNumber, delay)
+
+		//delay for a bit
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		if rnd <= 2 {
+			msg = fmt.Sprintf("*** We ran out of ingredients for pizza #%d!", pizzaNumber)
+		} else if rnd <= 4 {
+			msg = fmt.Sprintf("*** The cook quit while making pizza #%d!", pizzaNumber)
+		} else {
+			success = true
+			msg = fmt.Sprintf("*** Pizza order #%d is ready!", pizzaNumber)
+		}
+
+		return &PizzaOrder{
+			pizzaNumber: pizzaNumber,
+			message:     msg,
+			success:     success,
+		}
+
+	}
+
+	return &PizzaOrder{
+		pizzaNumber: pizzaNumber,
+	}
+}
+
 func pizzaria(pizzaMaker *Producer) {
 	// keep track of which pizza we are making
+	var i = 0
 
 	// run forever or until we receive a quit notification
 	// try to make pizzas
 	for {
+		currentPizza := makePizza(i)
 		// try to make a pizza
 		// decision
+
 	}
 }
 
