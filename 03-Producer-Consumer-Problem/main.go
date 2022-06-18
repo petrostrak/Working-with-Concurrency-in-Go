b8410ecd0260a7aad53e2bf2320cd357c05abe92
petrostrak <pit.trak@gmail.com>
2022-06-18 15:01:04 +0300

    Add Close method

--- 03-Producer-Consumer-Problem/main.go
+++ 03-Producer-Consumer-Problem/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
 const (
 	numberOfPizzas = 10
 )
@@ -19,14 +26,32 @@ type PizzaOrder struct {
 	success     bool
 }
 
+func (p *Producer) Close() error {
+	ch := make(chan error)
+
+	p.quit <- ch
+
+	return <-ch
+}
+
+func pizzaria(pizzaMaker *Producer) {}
+
 func main() {
 	// seed the random number generator
+	rand.Seed(time.Now().UnixNano())
 
 	// print out a message
+	color.Cyan("The Pizzaria is open for business!")
+	color.Cyan("----------------------------------")
 
 	// create a producer
+	pizzaJob := &Producer{
+		data: make(chan PizzaOrder),
+		quit: make(chan chan error),
+	}
 
 	// run the producer in the background
+	go pizzaria(pizzaJob)
 
 	// create and run a consumer (customers)
 
